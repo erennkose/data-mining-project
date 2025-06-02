fe7565d308ec3ad1fb9a7bd870756244f3031387
Eren Köse <erenkosee471@gmail.com>
2025-06-02 13:36:30 +0300

    fixes

--- src/main.py
+++ src/main.py
@@ -3,15 +3,13 @@ import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import (
-    classification_report, confusion_matrix, roc_auc_score, roc_curve,
+    confusion_matrix, roc_auc_score, roc_curve,
     accuracy_score, precision_score, recall_score, f1_score
 )
 from sklearn.preprocessing import label_binarize
 import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
-
-# Gereksiz uyarıları bastır
 warnings.filterwarnings('ignore')
 
 # ==============================================
